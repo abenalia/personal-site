cccb1421c399e8f7ff7ef762335ce00578dc86e8
Adam Benalia <79816337+abenalia@users.noreply.github.com>
2025-07-10 16:29:03 -0400

    changes

--- src/components/sections/hero.js
+++ src/components/sections/hero.js
@@ -61,7 +61,7 @@ const Hero = () => {
 
   const one = <h1>Hi, my name is</h1>;
   const two = <h2 className="big-heading">Adam Benalia.</h2>;
-  const three = <h3 className="big-heading">I make cool things.</h3>;
+  const three = <h3 className="big-heading">Engineer In The Making.</h3>;
   const four = (
     <>
       <p>
