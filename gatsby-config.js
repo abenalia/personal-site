2ce53f13bad52919d7d023b43e4851a7406f862c
Adam Benalia <79816337+abenalia@users.noreply.github.com>
2025-07-10 11:11:40 -0400

    changes

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,10 +2,9 @@ const config = require('./src/config');
 
 module.exports = {
   siteMetadata: {
-    title: 'Brittany Chiang',
-    description:
-      'Brittany Chiang is a software engineer who specializes in building (and occasionally designing) exceptional digital experiences.',
-    siteUrl: 'https://brittanychiang.com', // No trailing slash allowed!
+    title: 'Adam Benalia',
+    description: 'Adam Benalia is an Electrical Engineer motivated to change the world',
+    siteUrl: 'https://adambenalia.com', // No trailing slash allowed!
     image: '/og.png', // Path to your image you placed in the 'static' folder
     twitterUsername: '@bchiang7',
   },
@@ -20,8 +19,8 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: 'Brittany Chiang',
-        short_name: 'Brittany Chiang',
+        name: 'Adam Benalia',
+        short_name: 'Adam Benalia',
         start_url: '/',
         background_color: config.colors.darkNavy,
         theme_color: config.colors.navy,
